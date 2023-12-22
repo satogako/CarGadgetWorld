a64c98d0f1cdde6de5cec4a20f8165ac754a98b8
Satogako <genevaq@ukr.net>
2023-12-22 20:46:24 +0000

    Add sorting by category and sorting by search

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductsList.as_view(), name='products'),
+    path('category/<category>/',
+         views.ProductsList.as_view(), name='products_by_category'),
     path('item/<slug:slug>', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,15 +1,51 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, reverse
 from django.views import generic
+from django.contrib import messages
 from .models import Product
-
 
 class ProductsList(generic.ListView):
     '''
-    Displays all products
+    Displays all products, performs sorting and searching
     '''
     model = Product
     template_name = 'products/products.html'
     context_object_name = 'products'
+
+    def get_queryset(self, **kwargs):
+        queryset = super().get_queryset()
+
+        category = self.kwargs.get('category')
+        query = self.request.GET.get('search', None)
+        sort = self.request.GET.get('sort')
+        order = self.request.GET.get('order')
+
+        if category:
+            queryset = queryset.filter(
+                category__name=category).distinct()
+
+        if query:
+            queryset = queryset.filter(name__icontains=query
+                                    ).distinct() | queryset.filter(
+                description__icontains=query).distinct()
+
+        elif query == '':
+            messages.error(self.request,
+                        "You didn't enter any search criteria.")
+
+            queryset = queryset.none()
+
+        if sort == 'name':
+            if order == 'asc':
+                queryset = queryset.order_by('name')
+            else:
+                queryset = queryset.order_by('-name')
+        elif sort == 'price':
+            if order == 'asc':
+                queryset = queryset.order_by('price')
+            else:
+                queryset = queryset.order_by('-price')
+
+        return queryset
     
 
 def product_detail(request, slug):
