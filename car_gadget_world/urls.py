e50d6c839b436dd9307adeff5b59db2ead0e64da
Satogako <genevaq@ukr.net>
2023-12-28 19:02:22 +0000

    Add shopping cart app, urls and template

--- car_gadget_world/urls.py
+++ car_gadget_world/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include("home.urls")),
-    path('products/', include('products.urls')),
+    path('', include('home.urls')),
+    path('car_accessories/', include('products.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
