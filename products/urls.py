9eb54adaa4f99c2eef19535250ab03a58655ab9b
Satogako <genevaq@ukr.net>
2024-01-13 14:29:19 +0000

    Product Admin - Deleting products

--- products/urls.py
+++ products/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('item/<slug:slug>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>', views.delete_product, name='delete_product'),
 ]
