023bd23f624d2cb5c4649a1da629255e445b604e
Satogako <genevaq@ukr.net>
2025-09-19 14:20:15 +0100

    Remove temporary admin creation endpoint

--- car_gadget_world/urls.py
+++ car_gadget_world/urls.py
@@ -18,8 +18,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import handler404
-# Temporary for creating an admin user
-from .views import create_admin_user
 
 
 urlpatterns = [
@@ -30,8 +28,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('wish_list/', include('wish_list.urls')),
-    # Temporary for creating an admin user
-    path('create-admin/', create_admin_user),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
